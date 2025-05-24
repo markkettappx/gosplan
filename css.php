7c57a335a58186916152d5ce3ec5ff6809d5e714
Markkettapp <markkettapp@gmail.com>
2025-05-24 09:32:27 -0400

    Gosplan

---
+++ css.php
@@ -0,0 +1,8 @@
+<style>
+h1 {
+text-align: center;
+}
+span {
+color: red;
+}
+</style>
---
+++ index.php
@@ -0,0 +1,4 @@
+<?php include "css.php"; ?>
+<h1><span>G</span>o<span>s</span>p<span>l</span>a<span>n</span></h1>
+
+
